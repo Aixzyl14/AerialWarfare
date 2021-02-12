3d8ca0004a207a69fee58e0ff023652018de31f1
Aixzyl14 <71727630+Aixzyl14@users.noreply.github.com>
2021-02-12 13:04:20 +0000

    scoreboard

---
+++ Scripts/Scoreboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int Score;
+    Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText = GetComponent<Text>();
+        ScoreText.text = Score.ToString();
+    }
+
+    public void ScoreHit(int Addedscore)
+    {
+        Score = Score + Addedscore;
+        ScoreText.text = Score.ToString();
+    }
+}
